bf26e3ce70eec72230a5af5e58ee88ac8a3d341e
benlac <benoit.lacombled@gmail.com>
2020-06-02 22:17:45 +0200

    Controlled field

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,17 +11,50 @@ import Counter from 'src/components/Counter';
 import './app.scss';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <div className="container">
-      <Form />
-      <Tasks />
-    </div>
-    <Aside />
-    <Counter />
-  </div>
-);
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    valueForm: '',
+  };
+
+  changeValue = (newValue) => {
+    console.log(newValue);
+    this.setState({
+      valueForm: newValue,
+    });
+  }
+
+  render() {
+    const { valueForm } = this.state;
+    return (
+      <div className="app">
+        <Header />
+        <div className="container">
+          <Form value={valueForm} newValue={this.changeValue} />
+          <Tasks />
+        </div>
+        <Aside />
+        <Counter />
+      </div>
+    );
+  }
+}
+
+// const App = () => (
+//   <div className="app">
+//     <Header />
+//     <div className="container">
+//       <Form />
+//       <Tasks />
+//     </div>
+//     <Aside />
+//     <Counter />
+//   </div>
+// );
 
 // == Export
 export default App;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './form.scss';
 
-const Form = () => (
-  <form className="form">
-    <input type="text" className="form__input" />
-  </form>
-);
+const Form = ({ value, newValue }) => {
+  const handleChange = (event) => {
+    newValue(event.target.value);
+  };
+
+  return (
+    <form className="form">
+      <input
+        type="text"
+        className="form__input"
+        value={value}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+Form.propTypes = {
+  value: PropTypes.string.isRequired,
+  newValue: PropTypes.func.isRequired,
+};
 
 export default Form;
